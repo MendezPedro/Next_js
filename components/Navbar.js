3d55412a5a66d8f92897a9c7471fa475d293e2a3
PedroM <pedro.m@desafiolatam.com>
2023-01-26 20:00:03 -0300

    fix navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <div className="navbar-brand" href="#">Navbar</div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -22,28 +22,15 @@ export default function Navbar() {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
         <Link href="/">
-          Home
+          <div className="nav-link active" aria-current="page"> Home</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link href="/blog">
-          blog
+          <div className="nav-link" href="/blog">blog</div>
           </Link>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="/blog">blog</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
+
       </ul>
 
     </div>
